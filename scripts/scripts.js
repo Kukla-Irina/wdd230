2ca7b5b91e11ce8964a7eecb7ad363a29c98a774
Kukla-Irina <kuk18002@byui.edu>
2022-09-17 16:29:15 +0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
-
-    let text = document.lastModified;
-    document.getElementById("updated").innerHTML = text;
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('year').textContent = year;
+document.getElementById('updated').textContent = document.lastModified;
