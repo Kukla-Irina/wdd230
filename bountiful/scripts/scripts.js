54fe016eaedd3f60058a0c02c83b1676add936d2
Kukla-Irina <kuk18002@byui.edu>
2022-12-13 23:22:38 +0300

    Up

--- bountiful/scripts/scripts.js
+++ bountiful/scripts/scripts.js
@@ -1,19 +1,27 @@
 //menu
 
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
 
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
 
 //update
 
 const date = new Date();
 const year = date.getFullYear();
-const day = date.getDay ();
-document.getElementById('year').textContent = year;
-document.getElementById('updated').textContent = document.lastModified;
+const day = date.getDay();
+document.getElementById("year").textContent = year;
+document.getElementById("updated").textContent = document.lastModified;
 
 // select the elements to manipulate (output to)
 const datefield = document.querySelector(".date");
@@ -21,7 +29,7 @@ const datefield = document.querySelector(".date");
 // derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
+  now
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
