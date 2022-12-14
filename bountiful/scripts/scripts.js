0fc5281e82b46ee5dcf8232e5ad874c1a8cf5f03
Kukla-Irina <kuk18002@byui.edu>
2022-12-15 00:10:41 +0300

    Up

--- bountiful/scripts/scripts.js
+++ bountiful/scripts/scripts.js
@@ -34,11 +34,28 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 
 //order
 
-const ordersDisplay = document.querySelector(".drink");
-const numOrders = Number(window.localStorage.getItem("order"));
-ordersDisplay.textContent = numOrders;
-
-
 function createOrder() {
-  localStorage.setItem("order", Number());
+
+  console.log('run');
+
+  //Check to see if the localstorage variable exists. If it does not, this is the first time coming to the page, and we need to initalize it to 0
+  if (localStorage.getItem("drink") === null) {
+    // Store
+    localStorage.setItem("drink", "0");
+  }
+
+  // Get the value from local storage
+  const value = parseInt(localStorage.getItem("drink"));
+
+  // Incrememtnt the count by 1
+  const newValue = value + 1
+
+  //Write the value back to local storage
+  localStorage.setItem("drink", newValue);
+
+
+  // Write the value to the div
+  document.getElementById("drink").innerHTML = newValue;
+
 }
+
