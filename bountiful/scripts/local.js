9afd7ddad791c4752d06b1915d259c40c1b1bc6a
Kukla-Irina <kuk18002@byui.edu>
2022-12-15 03:48:00 +0300

    Up

---
+++ bountiful/scripts/local.js
@@ -0,0 +1,22 @@
+//order
+
+function createOrder() {
+
+    console.log('run');
+  
+    //Check to see if the localstorage variable exists. If it does not, this is the first time coming to the page, and we need to initalize it to 0
+    if (localStorage.getItem("drink") === null) {
+      // Store
+      localStorage.setItem("drink", "0");
+    }
+  
+    // Get the value from local storage
+    const value = parseInt(localStorage.getItem("drink"));
+  
+    // Incrememtnt the count by 1
+    const newValue = value + 1
+  
+    //Write the value back to local storage
+    localStorage.setItem("drink", newValue);
+  
+  }
--- bountiful/scripts/scripts.js
+++ bountiful/scripts/scripts.js
@@ -32,27 +32,3 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
   now
 );
 
-//order
-
-function createOrder() {
-
-  console.log('run');
-
-  //Check to see if the localstorage variable exists. If it does not, this is the first time coming to the page, and we need to initalize it to 0
-  if (localStorage.getItem("drink") === null) {
-    // Store
-    localStorage.setItem("drink", "0");
-  }
-
-  // Get the value from local storage
-  const value = parseInt(localStorage.getItem("drink"));
-
-  // Incrememtnt the count by 1
-  const newValue = value + 1
-
-  //Write the value back to local storage
-  localStorage.setItem("drink", newValue);
-
-}
-const displayOrders = localStorage.getItem('drink');
-document.getElementById("drink").innerHTML = displayOrders;
